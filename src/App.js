c9f7cc85acfa02177a925d24e86dce529eb5cf76
hellenkuttery <helen@clarusway.com>
2023-09-02 09:02:16 +0300

    add astrology setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import Header from "./components/Header/Header"
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     < Navbar/>
+       <Header/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ullam.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <h1 className="astrology">ASTROLOGY</h1>
+    </div>
+  )
+}
+export  default Header;
